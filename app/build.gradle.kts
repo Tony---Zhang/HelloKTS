5c1d11658b6895e0ce6292489542b18bb4b48d4c
Zhang Shuai <520zhangshuai@gmail.com>
2019-09-12 09:51:23 +0800

    [Shuai] config android lint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,13 @@ android {
             isMinifyEnabled = BuildTypeDebug.isMinifyEnabled
         }
     }
+    lintOptions {
+        isIgnoreTestSources = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 dependencies {
--- build.gradle.kts
+++ build.gradle.kts
@@ -23,3 +23,18 @@ allprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir, "$rootDir/buildSrc/build")
 }
+
+
+task("staticCheck") {
+    description = """Mimics all static checks that run on CI.
+        Note that this task is intended to run locally (not on CI), because on CI we prefer to have parallel execution
+        and separate reports for each check (multiple statuses eg. on github PR page).
+    """.trimMargin()
+    group = "verification"
+
+    afterEvaluate {
+        val linkTasks = subprojects.mapNotNull { "${it.name}:lintDebug" }
+        val dependTasks = listOf(linkTasks)
+        dependsOn(dependTasks)
+    }
+}
