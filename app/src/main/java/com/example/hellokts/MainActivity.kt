fdb378ae9c73405ab4c4df631e10efb86a6223e3
Zhang Shuai <520zhangshuai@gmail.com>
2019-09-25 10:24:03 +0800

    [Shuai] add sample proto

--- app/src/main/java/com/example/hellokts/MainActivity.kt
+++ app/src/main/java/com/example/hellokts/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.hellokts
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tutorial.Msg
+import com.example.tutorial.SecondMsg
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val msg = Msg.newBuilder()
+            .setFoo("foo")
+            .setBlah(SecondMsg.newBuilder().setBlah(1).build())
+            .build()
+
+        println("msg: ${msg.foo}, blah: ${msg.blah.blah}")
     }
 }
